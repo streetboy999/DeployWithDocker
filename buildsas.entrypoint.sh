7f80ef8840a2fd42695c32d9b51ab5c779f87230
Chunwei Wu <cwwu@foxmail.com>
2017-06-22 01:14:41 +0200

    1. Add sudo and tcsh into ubuntu:17.04.v2 (for SAS previously) and build a new image ubuntu:17.04.v4. Now both LSF and SAS use the same image 2. Configure sudo (put lsfadmin into sudoers file) 3. Resolve "no tty" issue while running docker exec directly through "script -q -c "/bin/bash" /dev/null 4. Modify dlogin script. Logon the docker container with lsfadmin by default 5. After logging docker containers both LSF and SAS environment are sourced automatically.

--- buildsas.entrypoint.sh
+++ buildsas.entrypoint.sh
@@ -27,6 +27,28 @@ function trapSignal(){
 	# Start ssh service
 	# Can ssh to remote node with root
 	sed -i '$a\PermitRootLogin yes' /etc/ssh/sshd_config
+	
+	# Configure sudo
+	cp -p /etc/sudoers /etc/sudoers.bak
+	chmod 640 /etc/sudoers
+	echo "lsfadmin ALL=(ALL:ALL) ALL" >> /etc/sudoers
+	sed -i "/secure_path/d" /etc/sudoers
+	sed -i "s/env_reset/\!env_reset/g" /etc/sudoers
+	chmod 440 /etc/sudoers
+	
+	# Create an automatic setup file. It has two functions:
+	# 1. Create a tty
+	# 2. Source LSF profile
+	mkdir /setup
+	setupFile="/setup/setup.sh"
+	touch $setupFile
+	chmod 777 $setupFile
+	echo '#!/bin/bash' >> $setupFile
+	echo '. $LSF_TOP/conf/profile.lsf' >> $setupFile
+	echo '. $JS_TOP/conf/profile.js' >> $setupFile
+	echo 'script -q -c "/bin/bash" /dev/null' >> $setupFile
+	
+	
 	service ssh start
 	
 	# Start LSF
